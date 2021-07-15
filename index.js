9a067d171d1385681cf880541ed8f245a3ecf466
eric <contreras.eric18@gmail.com>
2021-07-15 10:23:19 -0400

    commit tree 85ea656136c7250a98567f8080fa666a9c95c5fc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const array = []
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        array[i] = "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
+    }
+        return array;
+}
+
+function countDown(num) {
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
